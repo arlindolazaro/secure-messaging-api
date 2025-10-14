12ca0d77194e9283491c34319e42e60e0f6d0257
arlindolazaro <arlindolazaro202@gmail.com>
2025-10-14 09:13:09 +0200

    chore(app): adiciona aplicação principal e configuração

---
+++ src/main/java/com/securemessaging/SecureMessagingApplication.java
@@ -0,0 +1,14 @@
+package com.securemessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.securemessaging")
+public class SecureMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureMessagingApplication.class, args);
+    }
+}
